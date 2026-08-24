d75d34da7192e72b01dcf1170013f6d824ae1046
AdrianGushin <agushin101@gmail.com>
2026-08-23 22:45:45 -0400

    make transpose more efficient

--- mttkrp/run_mttkrp.jl
+++ mttkrp/run_mttkrp.jl
@@ -108,9 +108,10 @@ function calculate_results(dataset, mtxs, results)
             D = rand(size(B)[3], 16)
         elseif kind == "sparse"
             B = TensorMarket.tnsread(mtx)
-            B = fsparse(B[1]..., B[2])
             if mtx == "data/fb-m.tns"
-                B = permutedims(B, (3, 2, 1))
+                B = fsparse(B[1][3], B[1][2], B[1][1], B[2])
+            else
+                B = fsparse(B[1]..., B[2])
             end
             C = sprand(size(B)[2], 16, 0.01)
             D = sprand(size(B)[3], 16, 0.01)
