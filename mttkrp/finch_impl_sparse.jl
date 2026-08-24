f33d7164cd3a56827d1ef5f777540de67fb9ceb3
AdrianGushin <agushin101@gmail.com>
2026-08-24 14:21:21 -0400

    revert to bytemap

--- mttkrp/finch_impl_sparse.jl
+++ mttkrp/finch_impl_sparse.jl
@@ -5,48 +5,38 @@ using SparseArrays
 
 function finch_impl(B, C, D, num_cpu)
     dev = cpu(:t, num_cpu)
-    w = Tensor(SparseByteMap(Element(0.0)))
     _B = Tensor(SparseList(SparseList(SparseList(Element(0.0)))), B)
     _C = Tensor(Dense(SparseList(Element(0.0))), C)
     _D = Tensor(Dense(SparseList(Element(0.0))), D)
     
-    _A = Tensor(Dense(Shard(dev, SparseList(Element(0.0)))))
+    _A = Tensor(Dense(Shard(dev, SparseByteMap(Element(0.0)))))
     time = @belapsed begin
-        (_A, _B, _C, _D, dev, w) = $(_A, _B, _C, _D, dev, w)
+        (_A, _B, _C, _D, dev) = $(_A, _B, _C, _D, dev)
         @finch mode = :fast begin
             _A .= 0
            for r = parallel(_, dev)
-                w .= 0
                 for k = _
                     for j = _
                         for i = _
-                            w[i] += _B[i, j, k] * _D[k, r] * _C[j, r]
+                            A[i, r] += _B[i, j, k] * _D[k, r] * _C[j, r]
                         end
                     end
-                end
-                for i = _
-                    _A[i, r] = w[i]
                 end
             end
         end
     end
 
-   _A = Tensor(Dense(Shard(dev, SparseList(Element(0.0)))))
-   w = Tensor(SparseByteMap(Element(0.0)))
+   _A = Tensor(Dense(Shard(dev, SparseByteMap(Element(0.0)))))
 
     @finch begin
         _A .= 0
         for r = parallel(_, dev)
-            w .= 0
             for k = _
                 for j = _
                     for i = _
                         w[i] += _B[i, j, k] * _D[k, r] * _C[j, r]
                     end
                 end
-            end
-            for i = _
-                _A[i, r] = w[i]
             end
         end
     end
