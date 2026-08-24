de583d84147d23c3a76c78193aa5e5f8c6561c8f
AdrianGushin <agushin101@gmail.com>
2026-08-24 00:31:54 -0400

    revert

--- mttkrp/run_mttkrp.jl
+++ mttkrp/run_mttkrp.jl
@@ -108,11 +108,7 @@ function calculate_results(dataset, mtxs, results)
             D = rand(size(B)[3], 16)
         elseif kind == "sparse"
             B = TensorMarket.tnsread(mtx)
-            if mtx == "data/fb-m.tns"
-                B = fsparse(B[1][3], B[1][2], B[1][1], B[2])
-            else
-                B = fsparse(B[1]..., B[2])
-            end
+            B = fsparse(B[1]..., B[2])
             C = sprand(size(B)[2], 16, 0.01)
             D = sprand(size(B)[3], 16, 0.01)
         else
